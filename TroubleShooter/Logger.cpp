7226921ca443537286b0768267362572f9206480
Haruspik <nikita890blinov@gmail.com>
2022-05-22 18:19:07 +0500

    wxThread

--- TroubleShooter/Logger.cpp
+++ TroubleShooter/Logger.cpp
@@ -5,7 +5,7 @@ Logger::Logger(wxString set_Ip, int set_Delay) {
     setDelay(set_Ip, set_Delay);
 }
 void Logger::deletelines() { //удаляем старые  строки
-    wxString filename = "D:\\Labs_n_Other\\3Vorob\\TS2\\ping_res.txt";
+    wxString filename = "ping_res.csv";
     wxTextFile filein;
     filein.Open(filename);
     filein.RemoveLine(0);
@@ -24,17 +24,17 @@ void Logger::WriteLog() //запись строки в файл
     wxDateTime now = wxDateTime::Now();
     wxString date1 = now.Format(wxT("%a %T"));
     wxFile file;
-    file.Open(wxT("D:\\Labs_n_Other\\3Vorob\\TS2\\ping_res.txt"), wxFile::write_append);
-    file.Write(Ip + " " + wxString::Format(wxT("%i"), Delay) + " " + date1 + "\n");
+    file.Open(wxT("ping_res.csv"), wxFile::write_append);
+    file.Write(Ip + ";" + wxString::Format(wxT("%i"), Delay) + ";" + date1 + "\n");
 
 }
 
 void Logger::Check() { //проверка кол-ва строк
     int num;
     wxTextFile file;
-    file.Open(wxT("D:\\Labs_n_Other\\3Vorob\\TS2\\ping_res.txt"));
+    file.Open(wxT("ping_res.csv"));
     num = file.GetLineCount();
-    if (num > 500) {
+    if (num > 3) {
         deletelines();
     }
 }
